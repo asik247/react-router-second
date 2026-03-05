7a4c8ce2ccc3d7a007a82f9caf39ebfca44417c6
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-06 00:00:01 +0600

    text added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ const Navbar = () => {
     return (
         <div>
             <h1>ima nav</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit consectetur consequuntur qui atque ex, hic maxime quis, non beatae iusto perferendis distinctio totam officia adipisci voluptate aliquam aliquid expedita voluptatibus.</p>
+            <button>clcikc</button>
         </div>
     );
 };
