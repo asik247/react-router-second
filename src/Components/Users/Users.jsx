feb03f940e9ca2092fd6c6a3faf2dd2b0b8fad37
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-06 05:40:11 +0600

    route setup

---
+++ src/Components/Users/Users.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <div>
+            <h1>users code here</h1>
+        </div>
+    );
+};
+
+export default Users;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './Components/Routs/Root.jsx'
+import About from './Components/About/About.jsx'
+
+// router code start here;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      { index: true, Component:About}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+
+    </RouterProvider>
   </StrictMode>,
 )
