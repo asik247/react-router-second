4fdd8513fb0aaeace9b9e89d863ef0cd8a1a9b10
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-06 06:48:18 +0600

    navgation use

--- src/Components/Routs/Root.jsx
+++ src/Components/Routs/Root.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Footer from '../Footer/Footer';
 
 const Root = () => {
+    const navgation = useNavigation()
+    const isNavgation = navgation.state ==='loading'
     return (
         <div className='flex flex-col max-h-screen'>
             <Navbar></Navbar>
+
             <div className='h-100vh'>
+                {
+                    isNavgation && <h1 className='font-bold text-2xl'>Loadding....</h1>
+                }
                  <Outlet></Outlet>
             </div>
             <Footer></Footer>
